30c2a2b1d27bffe3dbc268c19ed86eda90e88691
infomanc3r <j.wyatt.ray@gmail.com>
2023-08-19 09:08:17 -0500

    Added refresh_session endpoint and JWT session revoke/refreshing using Guardian.

--- lib/elixir_first_phoenix_project_web/auth/error_response.ex
+++ lib/elixir_first_phoenix_project_web/auth/error_response.ex
@@ -5,3 +5,7 @@ end
 defmodule ElixirFirstPhoenixProjectWeb.Auth.ErrorResponse.Forbidden do
   defexception [message: "Forbidden", plug_status: 403]
 end
+
+defmodule ElixirFirstPhoenixProjectWeb.Auth.ErrorResponse.NotFound do
+  defexception [message: "Not Found", plug_status: 404]
+end
--- lib/elixir_first_phoenix_project_web/controllers/account_controller.ex
+++ lib/elixir_first_phoenix_project_web/controllers/account_controller.ex
@@ -44,10 +44,27 @@ defmodule ElixirFirstPhoenixProjectWeb.AccountController do
     end
   end
 
-  def sign_out(conn, %{})
+  def refresh_session(conn, %{}) do
+    old_token = Guardian.Plug.current_token(conn)
+    case Guardian.decode_and_verify(old_token) do
+      {:ok, claims} ->
+        case Guardian.resource_from_claims(claims) do
+          {:ok, account} ->
+            {:ok, _old, {new_token, _new_claims}} = Guardian.refresh(old_token)
+            conn
+            |> Plug.Conn.put_session(:account_id, account.id)
+            |> put_status(:ok)
+            |> render("account_token.json", %{account: account, token: new_token})
+          {:error, _reason} -> raise ErrorResponse.NotFound, message: "Account not found."
+        end
+      {:error, _reason} -> raise ErrorResponse.NotFound, message: "Token not found."
+    end
+  end
+
+  def sign_out(conn, %{}) do
     account = conn.assigns[:account]
     token = Guardian.Plug.current_token(conn)
-    Guardian.revoke!(token, account)
+    Guardian.revoke(token, account)
     conn
     |> Plug.Conn.clear_session()
     |> put_status(:ok)
